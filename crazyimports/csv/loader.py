7d6deac4b07152caeb568b9235369a2d8d4a7ee5
mishankov <mishankov@mail.com>
2020-12-19 00:28:29 +0300

    fix csv

--- crazyimports/csv/loader.py
+++ crazyimports/csv/loader.py
@@ -4,9 +4,12 @@ from crazyimports.common import ExDataLoader
 
 class CSV(ExDataLoader):
     def exec_module(self, mod):
-        data = self.load_data(open(mod.__spec__.origin, newline="").read())
-        self.repack(mod, data)
+        mod.data = []
+        with open(mod.__spec__.origin, newline="") as f:
+            reader = csv.reader(f)
+            mod.raw_data = reader
+            for row in reader:
+                mod.data.append(row)
 
     def load_data(self, data):
-        data = csv.reader(data, delimiter=",")
-        return {"data": data}
+        return {"data": [row for row in csv.reader(data)]}
