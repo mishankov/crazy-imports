5a6f37da633b9479cea3fa785a122b0e7117cf10
Denis Mishankov <mishankov@mail.com>
2021-10-10 12:10:47 +0300

    Python 3.10 updates (#78)
    * Update ci.yml
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     extras_require={"yaml": ["PyYAML==5.4.1"]},
 )
